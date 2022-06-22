8e06da60ea57f152ed135e6eeaee619ade4598e2
n-rick <e.ficadiere@gmail.com>
2022-06-22 11:43:47 +0200

    chore: add jwt

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordEncoder)
+    {
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('john@wick.us');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->hashPassword(
+            $user,
+            'wick'
+        ));
+        $manager->persist($user);
+        $user2 = new User();
+        $user2->setEmail('jack@dalton.us');
+        $user2->setPassword($this->passwordEncoder->hashPassword(
+            $user2,
+            'dalton'
+        ));
+        $manager->persist($user2);
+        $manager->flush();
+    }
+}
